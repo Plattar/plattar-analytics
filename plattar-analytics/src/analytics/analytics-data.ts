078fe4cf48a62db246bd0b4865840efe551315f9
David Arayan <david.arayan12@gmail.com>
2021-09-21 11:15:23 +1000

    - Added Analytics.write() functionality - Added Analytics.startRecordEngagement() functionality

---
+++ plattar-analytics/src/analytics/analytics-data.ts
@@ -0,0 +1,53 @@
+import { Util } from "../util/util";
+
+export class AnalyticsData {
+    private readonly _map: Map<string, any>;
+
+    constructor() {
+        this._map = new Map<string, any>();
+
+        this.push("source", "embed");
+        this.push("pageTitle", document.title);
+        this.push("pageURL", location.href);
+        this.push("referrer", document.referrer);
+        this.push("user_id", AnalyticsData._GetUserID());
+    }
+
+    public push(key: string, value: any): void {
+        this._map.set(key, value);
+    }
+
+    public get data(): any {
+        return Object.fromEntries(this._map);
+    }
+
+    private static _GetUserID(): string {
+        const key: string = "plattar_user_id";
+        let userID: string | null = null;
+
+        try {
+            userID = localStorage.getItem(key);
+        }
+        catch (err) {
+            userID = Util.generateUUID();
+
+            // try storing if just generated
+            try {
+                localStorage.setItem(key, userID);
+            }
+            catch (_err) {/* silent */ }
+        }
+
+        if (!userID) {
+            userID = Util.generateUUID();
+
+            // try storing if just generated
+            try {
+                localStorage.setItem(key, userID);
+            }
+            catch (_err) { /*silent */ }
+        }
+
+        return userID;
+    }
+}
--- plattar-analytics/src/analytics/analytics.ts
+++ plattar-analytics/src/analytics/analytics.ts
@@ -1,12 +1,37 @@
 import BasicHTTP from "../util/basic-http";
+import { AnalyticsData } from "./analytics-data";
 
 export class Analytics {
     private readonly _applicationID: string;
+    private readonly _handlePageHide: () => void;
+    private _pageTime: Date | null = null;
     public origin: "production" | "staging" | "dev" = "production";
     public event: "track" | "pageview" = "track";
 
     constructor(applicationID: string) {
         this._applicationID = applicationID;
+
+        this._handlePageHide = () => {
+            if (document.visibilityState === "hidden") {
+                this._pageTime = new Date();
+            }
+            else if (this._pageTime) {
+                const time2 = new Date();
+                const diff = time2.getTime() - this._pageTime.getTime();
+
+                const data: AnalyticsData = new AnalyticsData();
+
+                data.push("eventAction", "View Time");
+                data.push("viewTime", diff);
+                data.push("eventLabel", diff);
+
+                this.write(data);
+
+                this._pageTime = null;
+
+                document.removeEventListener("visibilitychange", this._handlePageHide, false);
+            }
+        };
     }
 
     public query(query: any | null | undefined = null): Promise<any> {
@@ -22,11 +47,39 @@ export class Analytics {
                 application_id: this._applicationID,
                 event: this.event,
                 data: query
-            }
+            };
 
             BasicHTTP.exec("POST", url, data).then((result) => {
                 accept(((result && result.results) ? result.results : {}));
             }).catch(reject);
         });
+    }
+
+    public write(data: AnalyticsData | null | undefined = null): Promise<any> {
+        return new Promise<any>((accept, reject) => {
+            if (!data) {
+                return reject(new Error("Analytics.write() - provided data was null"));
+            }
+
+            const url: string = this.origin === "dev" ? "http://localhost:9000/2015-03-31/functions/function/invocations" : "https://oyywgrj9ki.execute-api.ap-southeast-2.amazonaws.com/main/analytics";
+
+            data.push("applicationId", this._applicationID);
+
+            const sendData = {
+                type: "write",
+                application_id: this._applicationID,
+                origin: this.origin,
+                event: this.event,
+                data: data.data
+            };
+
+            BasicHTTP.exec("POST", url, sendData).then((result) => {
+                accept(((result && result.results) ? result.results : {}));
+            }).catch(reject);
+        });
+    }
+
+    public startRecordEngagement(): void {
+        document.addEventListener("visibilitychange", this._handlePageHide, false);
     }
 }
